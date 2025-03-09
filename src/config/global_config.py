8d2785274454e0c8875c5e54c78bc0c2e288bcdf
Teja Chava <tejachava80@gmail.com>
2025-03-08 20:50:38 -0800

    Initial commit: Rename project from autodev to rubberduck

--- src/config/global_config.py
+++ src/config/global_config.py
@@ -16,7 +16,17 @@ GLOBAL_CONFIG = GlobalConfig(
     QWEN_API_KEY=os.getenv("QWEN_API_KEY"),
     # pytest-dev/pytest is the easiest repo to start with
     INSTANCE_ID="pytest-dev__pytest-10051",
-    MODEL_NAME="qwen-max",
+    # Default model configuration:
+    #   claude-3-5-sonnet-20241022
+    #   deepseek-chat
+    #   deepseek-reasoner
+    #   gemini-1.5-pro
+    #   gpt-4o
+    #   gpt-4o-mini
+    #   o3-mini
+    #   qwen-max
+    #   qwen-plus
+    MODEL_NAME="gpt-4o-mini",
     SOURCEGRAPH_ENDPOINT="https://codeon.sourcegraph.app/.api/",
     SOURCEGRAPH_GQL_URL="https://codeon.sourcegraph.app/.api/graphql",
     SOURCEGRAPH_API_TOKEN=os.getenv("SOURCEGRAPH_API_TOKEN"),
